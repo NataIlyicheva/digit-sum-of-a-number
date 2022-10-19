a2de71db324a33af820dfe3c9866d0b71693a2ef
Natali <n7483205@ya.ru>
2022-10-19 10:23:05 +0700

    digit sum of a number

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+string num = Console.ReadLine()!;
+int len = num.Length;
+int[] value = new int[len];
+int count = 0;
+int sum = 0;
+
+while (count < len)
+{
+    value[count] = Convert.ToInt32(num[count].ToString());
+    count++;
+}
+    for (count = 0; count < len; count++)
+    sum += value[count];
+
+Console.WriteLine(sum);
